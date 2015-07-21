71518cc01b645eca158202a63bd56a91c6699f1c
Lawrence D'Anna <larry@elder-gods.org>
2015-07-21 01:47:21 -0700

    feeds-install

---
+++ feeds-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./scripts/feeds install \
+	luci tmux ubi-utils dmx_usb_module bind avahi expat libdaemon \
+	gdbm intltool dbus luci-ssl luci-app-ddns ddns-scripts \
+	luci-app-openvpn socat bash postfix avahi-autoipd
