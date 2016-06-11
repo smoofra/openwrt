f41b6276c34a3763ae42164b0e37c35ba53e05bf
Lawrence D'Anna <larry@elder-gods.org>
2016-06-10 22:59:01 -0400

    minicom

--- feeds-install.sh
+++ feeds-install.sh
@@ -3,4 +3,4 @@
 ./scripts/feeds install \
 	luci tmux ubi-utils dmx_usb_module bind avahi expat libdaemon \
 	gdbm intltool dbus luci-ssl luci-app-ddns ddns-scripts \
-	luci-app-openvpn socat bash postfix avahi-autoipd
+	luci-app-openvpn socat bash postfix avahi-autoip minicom
